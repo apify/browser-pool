82da7a1adc750640d7b70d7f090c70093b573263
Petr Patek <petrpatekml@gmail.com>
2020-12-11 23:12:24 +0100

    added test for index.js

---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+const BrowserControllerContext = require('../src/browser-controller-context');
+const BrowserPool = require('../src/browser-pool');
+const BrowserController = require('../src/abstract-classes/browser-controller');
+const BrowserPlugin = require('../src/abstract-classes/browser-plugin');
+
+const PuppeteerPlugin = require('../src/browser-plugins/puppeteer-plugin');
+const PlaywrightPlugin = require('../src/browser-plugins/playwright-plugin.js');
+
+const modules = require('../src/index');
+
+describe('Exports', () => {
+    test('Modules', () => {
+        expect(modules.BrowserControllerContext).toEqual(BrowserControllerContext);
+        expect(modules.BrowserPool).toEqual(BrowserPool);
+
+        expect(modules.PuppeteerPlugin).toEqual(PuppeteerPlugin);
+        expect(modules.PlaywrightPlugin).toEqual(PlaywrightPlugin);
+
+        expect(modules.BrowserController).toEqual(BrowserController);
+        expect(modules.BrowserPlugin).toEqual(BrowserPlugin);
+    });
+});
