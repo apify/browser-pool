c7d496a6e193d4aeea87d8e1e20cee5d7e74c939
Vlad Frangu <kingdgrizzle@gmail.com>
2021-04-28 12:32:35 +0300

    chore: shove a ts-ignore

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,7 @@
 import defaultLog from "@apify/log";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore For now till the logger becomes its own module
 export const log = defaultLog.child({
     prefix: "BrowserPool",
 });
