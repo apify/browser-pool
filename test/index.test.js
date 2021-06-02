43ecf671a593fcaa10cd0e198957763c8db5123c
Martin Adámek <banan23@gmail.com>
2021-06-02 15:45:10 +0200

    chore: fix linting errors

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const BrowserPool = require('../src/browser-pool');
 const PuppeteerPlugin = require('../src/puppeteer/puppeteer-plugin');
-const PlaywrightPlugin = require('../src/playwright/playwright-plugin.js');
+const PlaywrightPlugin = require('../src/playwright/playwright-plugin');
 
 const modules = require('../src/index');
 
