c57ade01bc1fd46f5c9021a86276a93f17d671ce
Vlad Frangu <kingdgrizzle@gmail.com>
2021-08-03 10:09:28 +0200

    chore: requested changes and remove useless tsignore

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,5 @@
-import defaultLog from "@apify/log";
+import defaultLog from '@apify/log';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore For now till the logger becomes its own module
 export const log = defaultLog.child({
-    prefix: "BrowserPool",
+    prefix: 'BrowserPool',
 });
