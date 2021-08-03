6cc4f1177b9a0a472a8aeec574dba55f4a33aa58
Vlad Frangu <kingdgrizzle@gmail.com>
2021-08-03 21:33:57 +0200

    refactor: convert launch context to TS (#34)
    * refactor: convert launch contex to TS
    
    * chore: rerun CI
    
    * feat: make LaunchContext's options generic
    Needed for https://github.com/apify/browser-pool/pull/24

---
+++ src/launch-context.ts
@@ -0,0 +1,107 @@
+import { join } from 'path';
+import { tmpdir } from 'os';
+import { nanoid } from 'nanoid';
+import type BrowserPlugin from './abstract-classes/browser-plugin';
+
+/**
+ * `LaunchContext` holds information about the launched browser. It's useful
+ * to retrieve the `launchOptions`, the proxy the browser was launched with
+ * or any other information user chose to add to the `LaunchContext` by calling
+ * its `extend` function. This is very useful to keep track of browser-scoped
+ * values, such as session IDs.
+ */
+export interface LaunchContextOptions<LibraryOptions> {
+    /**
+     * To make identification of `LaunchContext` easier, `BrowserPool` assigns
+     * the `LaunchContext` an `id` that's equal to the `id` of the page that
+     * triggered the browser launch. This is useful, because many pages share
+     * a single launch context (single browser).
+     */
+    id?: string;
+    /**
+     * The `BrowserPlugin` instance used to launch the browser.
+     */
+    browserPlugin: BrowserPlugin;
+    /**
+     * The actual options the browser was launched with, after changes.
+     * Those changes would be typically made in pre-launch hooks.
+     */
+    launchOptions: LibraryOptions;
+    /**
+     * By default pages share the same browser context.
+     * If set to `true` each page uses its own context that is destroyed once the page is closed or crashes.
+     */
+    useIncognitoPages?: boolean;
+    /**
+     * Path to a User Data Directory, which stores browser session data like cookies and local storage.
+     */
+    userDataDir?: string;
+    proxyUrl?: string;
+}
+
+export class LaunchContext<LibraryOptions> {
+    id?: string;
+
+    browserPlugin: BrowserPlugin;
+
+    launchOptions: LibraryOptions;
+
+    useIncognitoPages: boolean;
+
+    userDataDir: string;
+
+    private _proxyUrl?: string;
+
+    private readonly _reservedFieldNames = [...Reflect.ownKeys(this), 'extend'];
+
+    constructor(options: LaunchContextOptions<LibraryOptions>) {
+        const {
+            id,
+            browserPlugin,
+            launchOptions,
+            proxyUrl,
+            useIncognitoPages,
+            userDataDir = join(tmpdir(), nanoid()),
+        } = options;
+
+        this.id = id;
+        this.browserPlugin = browserPlugin;
+        this.launchOptions = launchOptions;
+        this.useIncognitoPages = useIncognitoPages ?? false;
+        this.userDataDir = userDataDir;
+
+        this._proxyUrl = proxyUrl;
+    }
+
+    /**
+     * Extend the launch context with any extra fields.
+     * This is useful to keep state information relevant
+     * to the browser being launched. It ensures that
+     * no internal fields are overridden and should be
+     * used instead of property assignment.
+     */
+    extend<T extends Record<PropertyKey, unknown>>(fields: T): void {
+        Object.entries(fields).forEach(([key, value]) => {
+            if (this._reservedFieldNames.includes(key)) {
+                throw new Error(`Cannot extend LaunchContext with key: ${key}, because it's reserved.`);
+            } else {
+                Reflect.set(this, key, value);
+            }
+        });
+    }
+
+    /**
+     * Sets a proxy URL for the browser.
+     * Use `undefined` to unset existing proxy URL.
+     */
+    set proxyUrl(url: string | undefined) {
+        this._proxyUrl = url && new URL(url).href;
+    }
+
+    /**
+     * Returns the proxy URL of the browser.
+     */
+    get proxyUrl(): string | undefined {
+        return this._proxyUrl;
+    }
+}
