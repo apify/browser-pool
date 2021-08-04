79c19970e6c091b9ee660bac20d539c4965d847d
Vlad Frangu <kingdgrizzle@gmail.com>
2021-08-04 14:54:25 +0200

    chore: convert events to enums

--- src/events.ts
+++ src/events.ts
@@ -1,12 +1,12 @@
-export const BROWSER_POOL_EVENTS = {
-    BROWSER_LAUNCHED: 'browserLaunched',
-    BROWSER_RETIRED: 'browserRetired',
-    BROWSER_CLOSED: 'browserClosed',
+export const enum BROWSER_POOL_EVENTS {
+    BROWSER_LAUNCHED = 'browserLaunched',
+    BROWSER_RETIRED = 'browserRetired',
+    BROWSER_CLOSED = 'browserClosed',
 
-    PAGE_CREATED: 'pageCreated',
-    PAGE_CLOSED: 'pageClosed',
-} as const;
+    PAGE_CREATED = 'pageCreated',
+    PAGE_CLOSED = 'pageClosed',
+}
 
-export const BROWSER_CONTROLLER_EVENTS = {
-    BROWSER_CLOSED: 'browserClosed',
-} as const;
+export const enum BROWSER_CONTROLLER_EVENTS {
+    BROWSER_CLOSED = 'browserClosed',
+};
