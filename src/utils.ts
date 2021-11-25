0a8f883c03f5804d269c6273241b7320e056cee9
Martin Adámek <banan23@gmail.com>
2021-11-25 17:40:27 +0100

    refactor: use cancellable timeouts via `@apify/timeout` (#62)
    * refactor: use cancellable timeouts via `@apify/timeout`
    
    Related:
    https://github.com/apify/apify-js/issues/1102
    https://github.com/apify/apify-js/issues/1216
    
    * chore: fix spacing in package.json
    
    * chore: require newer @apify/timeout
    
    * chore: adjust rootDir for tests
    
    * chore: add test for abortable timeouts
    
    * refactor: do not try to close inactive browser controllers

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,6 @@
 import type { PlaywrightPlugin, PuppeteerPlugin } from '.';
 import type { BrowserPlugin } from './abstract-classes/browser-plugin';
 
-export function addTimeoutToPromise<T>(promise: Promise<T>, timeoutMillis: number, errorMessage: string): Promise<T> {
-    return new Promise(async (resolve, reject) => { // eslint-disable-line
-        const timeout = setTimeout(() => reject(new Error(errorMessage)), timeoutMillis);
-        try {
-            const data = await promise;
-            resolve(data);
-        } catch (err) {
-            reject(err);
-        } finally {
-            clearTimeout(timeout);
-        }
-    });
-};
-
 export type UnwrapPromise<T> = T extends PromiseLike<infer R> ? UnwrapPromise<R> : T;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
